bcf1b0d4d37f5f640370f84096195397fa1cd539
joaoneto-hub <joaorodrigues8182@gmail.com>
2024-01-06 11:19:31 -0300

    Feat: controller de igredientes salvando

--- src/controllers/IgredientsController.ts
+++ src/controllers/IgredientsController.ts
@@ -3,8 +3,38 @@ import { Request, Response } from "express";
 import IngredientSchema, { IIngredient } from "../models/Igredients";
 
 class IgredientsController {
-  async get(req: Request, res: Response) {}
-  async post(req: Request, res: Response) {}
+  async get(req: Request, res: Response): Promise<void> {}
+  async post(req: Request, res: Response): Promise<void> {
+    const { name, quantity, value } = req.body;
+
+    if (!name) {
+      res.status(401).send({ error: "Nome e obrigatória" });
+    }
+    if (!quantity) {
+      res.status(401).send({ error: "Quantidade e obrigatória" });
+    }
+    if (!value) {
+      res.status(401).send({ error: "Valor e obrigatória" });
+    }
+
+    const Igredients = new IngredientSchema({
+      name,
+      quantity,
+      value,
+    });
+
+    try {
+      await Igredients.save();
+
+      res.status(201).json({ message: "Igrediente criado com sucesso!" });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Ocorreu um erro no servidor, tente mais tarde!" });
+    }
+  }
   async patch(req: Request, res: Response) {}
   async delete(req: Request, res: Response) {}
 }
+
+export default new IgredientsController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,25 @@
 // authRoutes.ts
 import express, { Router } from "express";
 import authController from "./controllers/AuthController";
+import IgredientsController from "./controllers/IgredientsController";
 
-const authRouter: Router = express.Router();
+const Route: Router = express.Router();
 
-authRouter.post("/auth/login", async (req, res) => {
+Route.post("/auth/login", async (req, res) => {
   await authController.login(req, res);
 });
 
-authRouter.post("/auth/register", async (req, res) => {
+Route.post("/auth/register", async (req, res) => {
   await authController.register(req, res);
 });
 
-// authRouter.get("/user/:id", async (req, res) => {
+// Route.get("/user/:id", async (req, res) => {
 //     await authController.getUser(req, res);
 // })
 
-export default authRouter;
+//Igredinets
+Route.post("/igredients", async (req, res) => {
+  await IgredientsController.post(req, res);
+});
+
+export default Route;
